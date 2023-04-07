cf4a965228004f8d3fd5d01b9d2350e7692139fd
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-04-07 12:57:28 -0700

    Update models.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,4 +2,90 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from projects.models import Project
 
+<<<<<<< Updated upstream
 # Create your models here.
+=======
+from accounts.utils import send_verification_email
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValidationError("Please enter an email")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        if email.endswith("@csun.edu"):
+            user.is_professor = True
+        elif email.endswith("@my.csun.edu"):
+            user.is_student = True
+        else:
+            raise ValidationError("Please use your CSUN email address")
+        user.set_password(password)
+        user.save()
+
+        try:
+            send_verification_email(None, user)  # calling send_verification_email
+        except Exception as e:
+            user.delete()  # delete user if email sending failed
+            raise ValidationError(str(e))
+
+        return user
+
+    def create_superuser(
+        self, email, first_name, last_name, password=None, **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields["first_name"] = first_name
+        extra_fields["last_name"] = last_name
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class CustomUser(AbstractUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True, default="")
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_professor = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email
+        super().save(*args, **kwargs)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    student_id = models.CharField(max_length=10, blank=True)
+    resume = models.FileField(
+        upload_to="resumes/",
+        validators=[FileExtensionValidator(["pdf"])],
+        null=True,
+        blank=True,
+    )
+
+    @receiver(post_save, sender=CustomUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+>>>>>>> Stashed changes
