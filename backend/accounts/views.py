e3ace31d940a417eb34f431faf5294a8ab4e383c
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-04-08 00:28:34 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,7 +1,9 @@
 from email.message import EmailMessage
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
+from accounts.forms import CustomRegistrationForm
+
 
 # Create your views here.
 
@@ -11,11 +13,18 @@ def index(request):
 
 
 def register(request):
+    form = CustomRegistrationForm()
+
     send_mail(
         subject="Register Your Account With CSUN.tech",
         message="Please register your account to continue.",
         from_email="no-reply@csun.tech",
-        recipient_list=["user@gmail.com"],
+        recipient_list=["TestUser@gmail.com"],
         fail_silently=True,
     )
-    return HttpResponse("Hello, world. You're at the register page.")
+
+    if request.method == "POST":
+        form = CustomRegistrationForm(request.POST)
+        print(form.data)
+
+    return render(request, "accounts/register.html", {"form": form})
