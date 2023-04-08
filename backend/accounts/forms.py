25294c0b5a3aaeea680c440c33d5e4e730de8e1f
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-04-08 00:28:12 -0700

    Create forms.py

---
+++ backend/accounts/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from accounts.models import CustomUser
+
+
+class CustomRegistrationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ["email", "password1", "password2"]
