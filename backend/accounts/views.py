21fb0ffcfe765f22e5954910acc24ed5df6c6674
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-04-07 22:42:25 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,21 @@
+from email.message import EmailMessage
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the index.")
+
+
+def register(request):
+    send_mail(
+        subject="Register Your Account With CSUN.tech",
+        message="Please register your account to continue.",
+        from_email="no-reply@csun.tech",
+        recipient_list=["user@gmail.com"],
+        fail_silently=True,
+    )
+    return HttpResponse("Hello, world. You're at the register page.")
