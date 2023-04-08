18d6ab971052ba83905223b76c6f98a249ae9c8b
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-04-07 22:42:29 -0700

    Update urls.py

--- backend/csuntech/urls.py
+++ backend/csuntech/urls.py
@@ -15,7 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from accounts import views as accounts_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", accounts_views.index, name="index"),
+    path("register/", accounts_views.register, name="register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
