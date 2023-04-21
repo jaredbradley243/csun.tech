4c52e70fe41cb537547a5271451bf06353262608
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-04-20 21:58:11 -0700

    Update admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -9,28 +9,37 @@ class CustomUserAdmin(UserAdmin):
         "email",
         "first_name",
         "last_name",
+        "student_id",
         "is_staff",
-        "is_active",
-        "is_student",
         "is_professor",
+        "is_team_lead",
+        "is_student",
+        "email_confirmed",
+        "is_volunteer",
+        "is_active",
     )
+
     list_filter = (
-        "email",
         "is_staff",
+        "is_professor",
+        "is_team_lead",
+        "is_student",
+        "email_confirmed",
         "is_active",
     )
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "student_id")}),
         (
             "Permissions",
             {
                 "fields": (
-                    "is_active",
                     "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
+                    "is_professor",
+                    "is_team_lead",
+                    "is_student",
+                    "email_confirmed",
+                    "is_active",
                 )
             },
         ),
@@ -45,16 +54,25 @@ class CustomUserAdmin(UserAdmin):
                     "email",
                     "first_name",
                     "last_name",
+                    "student_id",
                     "password1",
                     "password2",
                     "is_staff",
                     "is_active",
+                    "is_team_lead",
                 ),
             },
         ),
     )
     search_fields = ("email",)
     ordering = ("email",)
+
+    def is_volunteer(self, obj):
+        return obj.userprofile.is_volunteer
+
+    is_volunteer.boolean = True
+    is_volunteer.short_description = "Volunteer"
+    is_volunteer.admin_order_field = "userprofile__is_volunteer"
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
