085ad999f7f43b85627846ab0bb482aa3c6e9c11
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-04-20 21:58:14 -0700

    Update forms.py

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -6,4 +6,18 @@ from accounts.models import CustomUser
 class CustomRegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ["email", "password1", "password2"]
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+            "student_id",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(CustomRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields["email"].widget.attrs.update({"placeholder": "Email"})
+        self.fields["first_name"].widget.attrs.update({"placeholder": "First Name"})
+        self.fields["last_name"].widget.attrs.update({"placeholder": "Last Name"})
+        self.fields["student_id"].widget.attrs.update({"placeholder": "Student ID"})
