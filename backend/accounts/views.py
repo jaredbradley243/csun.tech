29bd2d7fe8f0c7a81cf6a953b68c60a1c345e64a
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-04-21 23:11:41 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -20,6 +20,8 @@ def register(request):
         form = CustomRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
+            # * is_active is set to False to prevent users from logging in
+            # * until they confirm their email
             user.is_active = False
             user_email = user.email
             if user_email.endswith("@csun.edu"):
@@ -28,6 +30,7 @@ def register(request):
                 user.is_student = True
             else:
                 return HttpResponse("Please enter a CSUN email!")
+            print(user.is_student, user.student_id)
             if user.is_student:
                 if not user.student_id:
                     return HttpResponse("Please enter your student ID")
@@ -62,5 +65,13 @@ def register(request):
                 recipient_list=[user_email],
                 fail_silently=True,
             )
+            # ! Uncomment the line below to save the user to the database
+            # ! after email confirmation is implemented and business logic is
+            # ! finalized
+            # user.save()
+        else:
+            # * If the form is not valid, return the error message
+            error_message = form.errors.as_text()
+            return HttpResponse(error_message)
 
     return render(request, "accounts/register.html", {"form": form})
