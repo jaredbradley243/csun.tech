4aefa06392504178e38ae712981ad4f97e1a83e7
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-05-24 23:09:31 -0700

    Added Todo

--- frontend/csuntech/src/pages/HomePage.jsx
+++ frontend/csuntech/src/pages/HomePage.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import SeniorDesignTable from "../components/SeniorDesignTable";
 import "./HomePage.css";
-import Header from "../layouts/Header";
+// TODO: Remove header import - Jared
+// import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
 
 export default function HomePage(props) {
-  const { isDarkMode, toggleDarkMode } = props;
+  const { isDarkMode } = props;
   return (
     <div
       className={
         isDarkMode ? "homePage_container darkMode" : "homePage_container"
       }
     >
-      <Header isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+      {/* // TODO: Remove Header. It will be used across the app in App.jsx */}
+      {/* <Header isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} /> */}
       <div className="designTable_container">
         <SeniorDesignTable />
       </div>
@@ -24,5 +26,6 @@ export default function HomePage(props) {
 
 HomePage.propTypes = {
   isDarkMode: PropTypes.bool.isRequired,
-  toggleDarkMode: PropTypes.func.isRequired,
+  // TODO: Remove toggle - Jared
+  // toggleDarkMode: PropTypes.func.isRequired,
 };
