2794905d6b5de26798d2c78ea887220574ee5386
Nyi Nyi Moe Htet <68096773+nyinyi2714@users.noreply.github.com>
2023-06-12 01:01:23 -0700

    Pagination of the Table

--- frontend/csuntech/src/pages/HomePage.jsx
+++ frontend/csuntech/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import SeniorDesignTable from "../components/SeniorDesignTable";
 import "./HomePage.css";
 import ProjectModal from "../components/ProjectModal";
@@ -10,6 +10,14 @@ export default function HomePage() {
   // the current project saved in this prop will be opened in the project Modal
   // setting it to null will close the Modal
   const [projectToOpen, setProjectToOpen] = useState(null);
+  // the current professor in this prop will be opened in the porfessor Modal
+  const [professorToOpen, setProfessorToOpen] = useState(null);
+  const currPage = useRef(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [currProjectList, setCurrProjectList] = useState([]);
+  const pageBtnContainer = useRef();
+  // Change this number to increase or decrease the num of projects displayed per page
+  const projectPerPage = 6;
   // Projects Mock Data from Backend
   const projects = [
     {
@@ -56,9 +64,139 @@ export default function HomePage() {
       skills: ["HTML", "CSS", "Javascript"],
       meetingTimes: "Tue, Wed 2-4pm",
     },
+    {
+      _id: 5,
+      name: "Project5",
+      professor: "professor5",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 6,
+      name: "Project6",
+      professor: "professor6",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 7,
+      name: "Project7",
+      professor: "professor7",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 8,
+      name: "Project8",
+      professor: "professor8",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 9,
+      name: "Project9",
+      professor: "professor9",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 10,
+      name: "Project10",
+      professor: "professor10",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 11,
+      name: "Project11",
+      professor: "professor11",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 12,
+      name: "Project12",
+      professor: "professor12",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 13,
+      name: "Project13",
+      professor: "professor13",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 14,
+      name: "Project14",
+      professor: "professor14",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 15,
+      name: "Project15",
+      professor: "professor15",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
+    {
+      _id: 16,
+      name: "Project16",
+      professor: "professor16",
+      openSpots: 3,
+      totalSpots: 30,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi. Adconsequatur facere sed!",
+      skills: ["HTML", "CSS", "Javascript"],
+      meetingTimes: "Tue, Wed 2-4pm",
+    },
   ];
-  // the current professor in this prop will be opened in the porfessor Modal
-  const [professorToOpen, setProfessorToOpen] = useState(null);
   // Professor Mock Data from Backend
   const professors = [
     {
@@ -104,8 +242,7 @@ export default function HomePage() {
   };
 
   const openProjectModal = (e) => {
-    // eslint-disable-next-line radix
-    const projectId = parseInt(e.target.id);
+    const projectId = parseInt(e.target.id, 10);
     const project = projects.find((p) => p._id === projectId);
     setProjectToOpen(project);
   };
@@ -116,14 +253,81 @@ export default function HomePage() {
     setProfessorToOpen(professor);
   };
 
+  function computeCurrProjectList() {
+    let list = [];
+    if (currPage.current === 1) list = projects.slice(0, projectPerPage);
+    else {
+      const startIndex = (currPage.current - 1) * projectPerPage;
+      list = projects.slice(startIndex, startIndex * 2);
+    }
+    setCurrProjectList(list);
+  }
+
+  function computeTotalPages() {
+    const total = Math.ceil(projects.length / projectPerPage);
+    setTotalPages(total);
+  }
+
+  useEffect(computeTotalPages, []);
+  useEffect(computeCurrProjectList, []);
+
+  function makePageBtnActive() {
+    const pageBtns = Array.from(pageBtnContainer.current.children);
+    pageBtns.forEach((btn) => {
+      if (parseInt(btn.getAttribute("data-btn-value"), 10) === currPage.current)
+        btn.classList.add("active");
+      else btn.classList.remove("active");
+    });
+  }
+
+  function changePage(e) {
+    const page = e.target.getAttribute("data-btn-value");
+    if (
+      (page === "prev" && currPage.current === 1) ||
+      (page === "next" && currPage.current === totalPages)
+    )
+      return;
+    if (page === "prev") currPage.current -= 1;
+    else if (page === "next") currPage.current += 1;
+    else currPage.current = parseInt(page, 10);
+    makePageBtnActive();
+    computeCurrProjectList();
+  }
+
+  function generatePageBtns() {
+    const buttons = [];
+    for (let i = 1; i <= totalPages; i += 1)
+      buttons.push(
+        <button
+          className={i === 1 ? "active" : ""}
+          key={i}
+          data-btn-value={i}
+          type="button"
+          onClick={changePage}
+        >
+          {i}
+        </button>
+      );
+    return buttons;
+  }
+
   return (
     <div className="homePage_container">
       <div className="designTable_container">
         <SeniorDesignTable
-          projects={projects}
+          projects={currProjectList}
           openProjectModal={openProjectModal}
           openProfessorModal={openProfessorModal}
         />
+      </div>
+      <div className="homePage_pagination" ref={pageBtnContainer}>
+        <button data-btn-value="prev" type="button" onClick={changePage}>
+          &laquo;
+        </button>
+        {generatePageBtns()}
+        <button data-btn-value="next" type="button" onClick={changePage}>
+          &raquo;
+        </button>
       </div>
       {projectToOpen && (
         <ProjectModal
