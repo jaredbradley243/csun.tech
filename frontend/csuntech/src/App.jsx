0ef3d68ff32c9f1dabce061134187213b1f71595
Nyi Nyi Moe Htet <68096773+nyinyi2714@users.noreply.github.com>
2023-06-16 18:27:52 -0700

    Created Custom TimePicker Component

--- frontend/csuntech/src/App.jsx
+++ frontend/csuntech/src/App.jsx
@@ -6,6 +6,7 @@ import ErrorPage from "./pages/ErrorPage";
 import Signup from "./pages/Signup";
 import HomePage from "./pages/HomePage";
 import Header from "./layouts/Header";
+import TimePicker from "./components/TimePicker";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(
@@ -25,6 +26,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<ErrorPage />} />
+        <Route path="/timepicker" element={<TimePicker />} />
       </Routes>
     </div>
   );
