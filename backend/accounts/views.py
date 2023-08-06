b343b5c42a1ad9d122b9101dadf74990cd444f2f
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-08-05 20:55:32 -0700

    Update views and urls

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -4,8 +4,43 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import CustomUser, UserProfile, StudentProfile
+from .serializers import (
+    CustomUserSerializer,
+    UserProfileSerializer,
+    StudentProfileSerializer,
+)
 
-# Create your views here.
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+
+class StudentProfileViewSet(viewsets.ModelViewSet):
+    queryset = StudentProfile.objects.all()
+    serializer_class = StudentProfileSerializer
+
+    @action(detail=True, methods=["post"])
+    def join_project(self, request, pk=None):
+        student_profile = self.get_object()
+        project_id = request.data.get("project_id")
+        project = Project.objects.get(pk=project_id)
+
+        try:
+            student_profile.join_project(project)
+            serializer = StudentProfileSerializer(student_profile)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except ValidationError as e:
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
 
 
 def index(request):
--- backend/csuntech/urls.py
+++ backend/csuntech/urls.py
@@ -23,14 +23,32 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from accounts import views as accounts_views
 
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from accounts.views import (
+    CustomUserViewSet,
+    UserProfileViewSet,
+    StudentProfileViewSet,
+)
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path("", accounts_views.index, name="index"),
+#     path("register/", accounts_views.register, name="register"),
+#     path(
+#         "login/",
+#         auth_views.LoginView.as_view(template_name="accounts/login.html"),
+#         name="login",
+#     ),
+#     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+# ]
+
+router = DefaultRouter()
+router.register(r"users", CustomUserViewSet)
+router.register(r"userprofiles", UserProfileViewSet)
+router.register(r"studentprofiles", StudentProfileViewSet)
 urlpatterns = [
+    path("", include(router.urls)),
     path("admin/", admin.site.urls),
-    path("", accounts_views.index, name="index"),
-    path("register/", accounts_views.register, name="register"),
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="accounts/login.html"),
-        name="login",
-    ),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
