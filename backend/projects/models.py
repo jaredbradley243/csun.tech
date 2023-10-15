9dc8e4e6db0d9e349cbe037d149dbc875f2226c9
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-10-14 19:21:11 -0700

    Added new meeting Schule model and validation

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
-
+from django.db.models import JSONField
 from rest_framework.exceptions import ValidationError, PermissionDenied
+from django.core.exceptions import ValidationError
+from datetime import datetime
 
 
 # TODO: Add validation to ensure that open_slots can never exceed capacity
@@ -11,7 +13,7 @@ class Project(models.Model):
     open_slots = models.IntegerField(validators=[MaxValueValidator(99)])
     capacity = models.IntegerField(validators=[MaxValueValidator(99)])
     relevant_skills = models.CharField(null=True, blank=True, max_length=500)
-    meeting_schedule = models.ManyToManyField("MeetingTime")
+    meeting_schedule = JSONField(null=True, blank=True)
 
     def __str__(self):
         return self.project_name
@@ -46,22 +48,53 @@ class Project(models.Model):
         self.open_slots += 1
         self.save()
 
+    def save(self, *args, **kwargs):
+        self.validate_meeting_schedule()
+        self.validate_open_slots()
+        super().save(*args, **kwargs)
 
-class DayofWeek(models.TextChoices):
-    MONDAY = "MON"
-    TUESDAY = "TUE"
-    WEDNESDAY = "WED"
-    THURSDAY = "THU"
-    FRIDAY = "FRI"
-    SATURDAY = "SAT"
-    SUNDAY = "SUN"
+    def validate_meeting_schedule(self):
+        if self.meeting_schedule is not None:
+            valid_days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+            if not isinstance(self.meeting_schedule, list):
+                raise ValidationError("meeting_schedule should be a list")
 
+            for meeting_dict in self.meeting_schedule:
+                if not isinstance(meeting_dict, dict):
+                    raise ValidationError(
+                        "Each item in meeting_schedule should be a dictionary"
+                    )
 
-# Todo: Add validation to ensure that start_time is earlier than end_time
-class MeetingTime(models.Model):
-    day_of_week = models.CharField(max_length=3, choices=DayofWeek.choices)
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+                required_keys = ["day", "start_time", "end_time"]
+                for key in required_keys:
+                    if key not in meeting_dict:
+                        raise ValidationError(
+                            f"{key} is required in each meeting dictionary"
+                        )
 
-    def __str__(self):
-        return f"{self.get_day_of_week_display()} {self.start_time.strftime('%H:%M')} - {self.end_time.strftime('%H:%M')}"
+                time_format = "%H:%M"  # 24-hour format
+                for time_key in ["start_time", "end_time"]:
+                    try:
+                        time_obj = datetime.strptime(
+                            meeting_dict[time_key], time_format
+                        )
+                        meeting_dict[time_key] = datetime.strftime(
+                            time_obj, time_format
+                        )
+                    except ValueError:
+                        raise ValidationError(
+                            f"{time_key} should be in {time_format} format"
+                        )
+
+                normalized_day = meeting_dict["day"].title()
+                if normalized_day not in valid_days:
+                    raise ValidationError(
+                        "day must be one of the valid days: {}".format(
+                            ", ".join(valid_days)
+                        )
+                    )
+                meeting_dict["day"] = normalized_day
+
+    def validate_open_slots(self):
+        if self.open_slots > self.capacity:
+            raise ValidationError("Open slots cannot exceed capacity")
