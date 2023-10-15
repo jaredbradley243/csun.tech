846bad7615e9a9f807731d3c69be02318255a9d7
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2023-10-14 19:21:40 -0700

    Add projects to admin

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project  # Import the Project model
 
-# Register your models here.
+# Define the admin class
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('project_name', 'open_slots', 'capacity')  # Customize which fields to display
+    list_filter = ('open_slots', 'capacity')  # Customize filters
+    search_fields = ('project_name',)  # Customize search fields
+
+# Register the admin class with the model
+admin.site.register(Project, ProjectAdmin)
