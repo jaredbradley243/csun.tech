17205c3ba6c31d916c259c23f72bd47476628f48
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2024-01-03 02:04:03 -0800

    Update urls for projects, professors, user profile, and token endpoints

--- backend/csuntech/urls.py
+++ backend/csuntech/urls.py
@@ -1,58 +1,55 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth
-from django.contrib import admin
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from accounts import views as accounts_views
-
-
-from django.urls import path, include
+from django.urls import include
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from accounts.views import (
     CustomUserViewSet,
-    #     # StudentProfileViewSet,
-    #     # ProfessorProfileViewSet,
-    #     # ProfessorDashboardViewSet,
-    #     # TeamLeadProfileViewSet,
+    StudentProfileViewSet,
+    ProfessorDashboardViewSet,
+    ProfessorProfileViewSet,
+    UserProfileViewSet,
     #     # RegisterViewSet,
 )
 from projects.views import ProjectsViewSet
 
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-#     path("", accounts_views.index, name="index"),
-#     path("register/", accounts_views.register, name="register"),
-#     path(
-#         "login/",
-#         auth_views.LoginView.as_view(template_name="accounts/login.html"),
-#         name="login",
-#     ),
-#     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-# ]
-
 router = DefaultRouter()
 router.register(r"users", CustomUserViewSet)
-# router.register(r"students", StudentProfileViewSet)
-# router.register(r"teamleads", TeamLeadProfileViewSet)
-# router.register(r"professors", ProfessorProfileViewSet)
+# router.register(r"userprofile", UserProfileViewSet, basename="userprofile")
+router.register(r"students", StudentProfileViewSet)
+router.register(r"professorprofiles", ProfessorProfileViewSet)
 router.register(r"projects", ProjectsViewSet)
-# router.register(
-#     r"professordashboard", ProfessorDashboardViewSet, basename="professordashboard"
-# )
 # router.register(r"register", RegisterViewSet, basename="register")
 
 urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls),
-    # path(
-    #     "professordashboard/<str:professor_id>/",
-    #     ProfessorDashboardViewSet.as_view({"get": "list"}),
-    #     name="professor-dashboard",
-    # ),
+    path(
+        "projects/students/<str:user_id>/",
+        ProjectsViewSet.as_view({"delete": "leave_project"}),
+    ),
+    path(
+        "professors/<uuid:pk>/dashboard/",
+        ProfessorDashboardViewSet.as_view({"get": "dashboard"}),
+    ),
+    path(
+        "professors/<uuid:pk>/students/",
+        ProfessorDashboardViewSet.as_view({"get": "students"}),
+    ),
+    path(
+        "professors/<uuid:pk>/projects/",
+        ProfessorDashboardViewSet.as_view({"get": "projects"}),
+    ),
+    path("userprofile/", UserProfileViewSet.as_view({"get": "retrieve_user_profile"})),
+    path(
+        "userprofile/<uuid:pk>",
+        UserProfileViewSet.as_view({"put": "update"}),
+    ),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 # TODO - Create email verification endpoint to send users when
