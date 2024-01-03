f3b8102913520edf0771d6b172335b387d23f541
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2024-01-03 02:04:49 -0800

    Update serializers for new endpoints

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,14 +1,11 @@
 from django.core.exceptions import ValidationError
-from django.db import IntegrityError, transaction
 from rest_framework import serializers
+from django.db import IntegrityError, transaction
 from .models import (
     CustomUser,
-    # StudentProfile,
-    # TeamLeadProfile,
+    StudentProfile,
     ProfessorProfile,
 )
-import logging
-from googlesearch import search
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -31,55 +28,23 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
 
 # #! Student profile always created with team lead false, even if specified otherwise
-# class StudentProfileSerializer(serializers.ModelSerializer):
-#     email = serializers.EmailField(write_only=True, help_text="Required")
-#     password = serializers.CharField(write_only=True, help_text="Required")
-#     first_name = serializers.CharField(source="user.first_name", read_only=True)
-#     last_name = serializers.CharField(source="user.last_name", read_only=True)
+class StudentProfileSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(write_only=True, help_text="Required")
+    password = serializers.CharField(write_only=True, help_text="Required")
+    first_name = serializers.CharField(source="user.first_name", read_only=True)
+    last_name = serializers.CharField(source="user.last_name", read_only=True)
 
-#     class Meta:
-#         model = StudentProfile
-#         fields = "__all__"
-#         extra_kwargs = {
-#             "student_id": {"help_text": "Required"},
-#             "user": {"read_only": True},
-#         }
-
-#     def create(self, validated_data):
-#         email = validated_data.get("email")
-#         password = validated_data.get("password")
-#         student_id = validated_data.get("student_id")
-#         if email and not email.endswith("@my.csun.edu"):
-#             raise serializers.ValidationError(
-#                 "This user is not a student or is not using a student email"
-#             )
-
-#         user = None
-#         try:
-#             user = CustomUser.objects.create_user(
-#                 email=email,
-#                 password=password,
-#             )
-
-#             if user.email.endswith("@my.csun.edu"):
-#                 student_profile = StudentProfile.objects.create(
-#                     user=user, student_id=student_id
-#                 )
-#         except (ValidationError, IntegrityError) as e:
-#             if user:
-#                 user.delete()
-#             raise serializers.ValidationError(str(e))
-
-#         return student_profile
-
-
-# class TeamLeadProfileSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = TeamLeadProfile
-#         fields = "__all__"
+    class Meta:
+        model = StudentProfile
+        fields = "__all__"
+        extra_kwargs = {
+            "student_id": {"help_text": "Required"},
+            "user": {"read_only": True},
+        }
 
 
 # * This serializer is nested in the ProjectDetailSerializer in /projects/serializers.py
+# * to show detailed professor information in the project detail
 class ProfessorProfileSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(source="user.first_name", read_only=True)
     last_name = serializers.CharField(source="user.last_name", read_only=True)
@@ -92,33 +57,9 @@ class ProfessorProfileSerializer(serializers.ModelSerializer):
             "user": {"read_only": True},
         }
 
-    # def create(self, validated_data):
-    #     csun_faculty_page_link = validated_data.get("csun_faculty_page_link")
-    #     if email and not email.endswith("@csun.edu"):
-    #         raise serializers.ValidationError(
-    #             "This user is not a professor or is not using a professor email"
-    #         )
-
-    #     user = None
-    #     try:
-    #         user = CustomUser.objects.create_user(
-    #             email=email,
-    #             password=password,
-    #         )
-
-    #         if user.email.endswith("@csun.edu"):
-    #             professor_profile = ProfessorProfile.objects.create(
-    #                 user=user, csun_faculty_page_link=csun_faculty_page_link
-    #             )
-    #     except (ValidationError, IntegrityError) as e:
-    #         if user:
-    #             user.delete()
-    #         raise serializers.ValidationError(str(e))
-
-    #     return professor_profile
-
 
 # * This serializer is nested in the ProjectListSerializer in /projects/serializers.py
+# * to show basic professor information in the project list and dashboard
 class ProfessorNameSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(source="user.first_name", read_only=True)
     last_name = serializers.CharField(source="user.last_name", read_only=True)
@@ -128,44 +69,143 @@ class ProfessorNameSerializer(serializers.ModelSerializer):
         fields = ["first_name", "last_name", "id"]
 
 
-# # * Used for Professor Dashboard
-# class CustomStudentProfileSerializer(serializers.ModelSerializer):
-#     first_name = serializers.CharField(source="user.first_name")
-#     last_name = serializers.CharField(source="user.last_name")
-#     email = serializers.EmailField(source="user.email")
+# * Used for Professor Dashboard
+class ProfessorDashboardStudentSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(source="user.first_name")
+    last_name = serializers.CharField(source="user.last_name")
+    email = serializers.EmailField(source="user.email")
+
+    class Meta:
+        model = StudentProfile
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "team_lead",
+            "is_volunteer",
+            "student_id",
+            "resume",
+            "user",
+            "project",
+        ]
+
+
+# class UserProfileStudentSerializer(serializers.ModelSerializer):
+#     team_lead = serializers.BooleanField(
+#         source="studentprofile.team_lead", read_only=True
+#     )
+#     volunteer = serializers.BooleanField(
+#         source="studentprofile.is_volunteer", read_only=True
+#     )
+#     student_id = serializers.CharField(
+#         source="studentprofile.student_id", read_only=True
+#     )
+#     resume = serializers.FileField(source="studentprofile.resume")
+#     project = serializers.CharField(source="studentprofile.project", read_only=True)
 
 #     class Meta:
-#         model = StudentProfile
+#         model = CustomUser
 #         fields = [
+#             "id",
 #             "first_name",
 #             "last_name",
 #             "email",
-#             "id",
-#             "team_lead",
-#             "is_volunteer",
 #             "student_id",
 #             "resume",
-#             "user",
+#             "team_lead",
+#             "volunteer",
 #             "project",
 #         ]
+#         extra_kwargs = {
+#             "id": {"read_only": True},
+#             "first_name": {"read_only": True},
+#             "last_name": {"read_only": True},
+#             "email": {"read_only": True},
+#             # team_lead: readonly specified in serializer
+#             # volunteer: readonly specified in serializer
+#             # student_id: readonly specified in serializer
+#             # Resume can be updated
+#             # project: readonly specified in serializer
+#         }
 
 
-# # * Used for Professor Dashboard
-# class CustomProfessorProfileSerializer(serializers.ModelSerializer):
-#     first_name = serializers.CharField(source="user.first_name")
-#     last_name = serializers.CharField(source="user.last_name")
-#     email = serializers.EmailField(source="user.email")
+class UserProfileStudentSerializer(serializers.ModelSerializer):
+    user_id = serializers.UUIDField(source="user.id", read_only=True)
+    first_name = serializers.CharField(source="user.first_name", read_only=True)
+    last_name = serializers.CharField(source="user.last_name", read_only=True)
+    email = serializers.EmailField(source="user.email", read_only=True)
+    project = serializers.StringRelatedField(read_only=True)
 
-#     class Meta:
-#         model = ProfessorProfile
-#         fields = [
-#             "first_name",
-#             "last_name",
-#             "email",
-#             "id",
-#             "rate_my_professor_link",
-#             "csun_faculty_page_link",
-#             "bio",
-#             "user",
-#             "projects",
-#         ]
+    class Meta:
+        model = StudentProfile
+        fields = [
+            "user_id",
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "student_id",
+            "resume",
+            "team_lead",
+            "is_volunteer",
+            "project",
+        ]
+        extra_kwargs = {
+            # user_id: readonly specified in serializer
+            "id": {"read_only": True},
+            # first_name: readonly specified in serializer
+            # last_name: readonly specified in serializer
+            # email: readonly specified in serializer
+            "student_id": {"required": False},
+            # Resume can be updated
+            "team_lead": {"read_only": True},
+            "is_volunteer": {"read_only": True},
+            # project: readonly specified in serializer
+        }
+
+    def update(self, instance, validated_data):
+        student_profile = instance.studentprofile
+        student_profile.student_id = validated_data.get(
+            "student_id", student_profile.student_id
+        )
+        student_profile.resume = validated_data.get("resume", student_profile.resume)
+        student_profile.save()
+        return super().update(instance, validated_data)
+
+
+class UserProfileProfessorSerializer(serializers.ModelSerializer):
+    user_id = serializers.UUIDField(source="user.id", read_only=True)
+    first_name = serializers.CharField(source="user.first_name", read_only=True)
+    last_name = serializers.CharField(source="user.last_name", read_only=True)
+    email = serializers.EmailField(source="user.email", read_only=True)
+    projects = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = ProfessorProfile
+        fields = [
+            "user_id",
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "bio",
+            "csun_faculty_page_link",
+            "projects",
+        ]
+        extra_kwargs = {
+            # user_id: readonly specified in serializer
+            "id": {"read_only": True},
+            # first_name: readonly specified in serializer
+            # last_name: readonly specified in serializer
+            # email: readonly specified in serializer
+            # Bio can be updated
+            "csun_faculty_page_link": {"read_only": True},
+            # project: readonly specified in serializer
+        }
+
+    def update(self, instance, validated_data):
+        professor_profile = instance.professorprofile
+        professor_profile.bio = validated_data.get("bio", professor_profile.bio)
+        professor_profile.save()
+        return super().update(instance, validated_data)
--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -2,102 +2,10 @@ from rest_framework import serializers
 from .models import Project
 from accounts.serializers import (
     ProfessorProfileSerializer,
-    CustomUserSerializer,
-    # CustomProfessorProfileSerializer,
     ProfessorNameSerializer,
 )
 
 from datetime import datetime
-
-
-# TODO - Add email validations
-# 1) Create verification token
-# 2) Send email to user with verification token
-# class ProjectSerializer(serializers.ModelSerializer):
-#     professors = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-#     class Meta:
-#         model = Project
-#         fields = "__all__"
-#         fields = [
-#             "id",
-#             "project_name",
-#             # "project_description",
-#             "open_slots",
-#             "capacity",
-#             # "relevant_skills",
-#             "meeting_schedule",
-#             "professors",
-#         ]
-#         extra_kwargs = {
-#             "project_name": {"help_text": "Required"},
-#             "open_slots": {"help_text": "Required"},
-#             "capacity": {"help_text": "Required"},
-#         }
-
-#     # TODO - Remove validate_meeting_schedule from serializer or model
-#     def validate_meeting_schedule(self, meeting_schedule_list):
-#         if meeting_schedule_list is None or len(meeting_schedule_list) == 0:
-#             return []
-
-#         valid_days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-#         if not isinstance(meeting_schedule_list, list):
-#             raise serializers.ValidationError("meeting_schedule should be a list")
-
-#         for meeting_dict in meeting_schedule_list:
-#             if not isinstance(meeting_dict, dict):
-#                 raise serializers.ValidationError(
-#                     "Each item in meeting_schedule should be a dictionary"
-#                 )
-
-#             required_keys = ["day", "start_time", "end_time"]
-#             for key in required_keys:
-#                 if key not in meeting_dict:
-#                     raise serializers.ValidationError(
-#                         f"{key} is required in each meeting dictionary"
-#                     )
-
-#             time_format = "%H:%M"  # 24-hour format
-#             for time_key in ["start_time", "end_time"]:
-#                 try:
-#                     time_obj = datetime.strptime(meeting_dict[time_key], time_format)
-#                     meeting_dict[time_key] = datetime.strftime(time_obj, time_format)
-#                 except ValueError:
-#                     raise serializers.ValidationError(
-#                         f"{time_key} should be in {time_format} format"
-#                     )
-
-#             normalized_day = meeting_dict["day"].title()
-#             if normalized_day not in valid_days:
-#                 raise serializers.ValidationError(
-#                     "day must be one of the valid days: {}".format(
-#                         ", ".join(valid_days)
-#                     )
-#                 )
-#             meeting_dict["day"] = normalized_day
-
-#         return meeting_schedule_list
-
-#     def validate(self, data):
-#         open_slots = data.get("open_slots", None)
-#         capacity = data.get("capacity", None)
-
-#         if open_slots is not None and capacity is not None:
-#             if open_slots > capacity:
-#                 raise serializers.ValidationError(
-#                     "open_slots should not exceed capacity"
-#                 )
-#         return data
-
-#     def create(self, validated_data):
-#         project = Project.objects.create(**validated_data)
-#         return project
-
-#     def update(self, instance, validated_data):
-#         for attr, value in validated_data.items():
-#             setattr(instance, attr, value)
-#         instance.save()
-#         return instance
 
 
 class ProjectListSerializer(serializers.ModelSerializer):
@@ -133,6 +41,7 @@ class ProjectDetailSerializer(serializers.ModelSerializer):
             "capacity": {"help_text": "Required"},
         }
 
+    # TODO - Implement validation that disallows start_time from being after end_time
     def validate_meeting_schedule(self, meeting_schedule_list):
         if meeting_schedule_list is None or len(meeting_schedule_list) == 0:
             return []
@@ -182,7 +91,7 @@ class ProjectDetailSerializer(serializers.ModelSerializer):
         if open_slots is not None and capacity is not None:
             if open_slots > capacity:
                 raise serializers.ValidationError(
-                    "open_slots should not exceed capacity"
+                    "open slots should not exceed capacity"
                 )
         return data
 
@@ -198,19 +107,19 @@ class ProjectDetailSerializer(serializers.ModelSerializer):
 
 
 # # * Used for Professor Dashboard
-# class CustomProjectSerializer(serializers.ModelSerializer):
-#     professors = CustomProfessorProfileSerializer(many=True, read_only=True)
-#     project_id = serializers.IntegerField(source="id", read_only=True)
+class ProfessorDashboardProjectSerializer(serializers.ModelSerializer):
+    professors = ProfessorNameSerializer(many=True, read_only=True)
+    # id = serializers.IntegerField(source="id", read_only=True)
 
-#     class Meta:
-#         model = Project
-#         fields = [
-#             "project_id",
-#             "project_name",
-#             "project_description",
-#             "open_slots",
-#             "capacity",
-#             "relevant_skills",
-#             "meeting_schedule",
-#             "professors",
-#         ]
+    class Meta:
+        model = Project
+        fields = [
+            "id",
+            "project_name",
+            "project_description",
+            "open_slots",
+            "capacity",
+            "relevant_skills",
+            "meeting_schedule",
+            "professors",
+        ]
