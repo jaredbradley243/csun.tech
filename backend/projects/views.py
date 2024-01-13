070361d5a3d14b29f9e9fe1db181383aa57f7335
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2024-01-12 23:02:05 -0800

    Add error handling to viewsets

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -31,6 +31,7 @@ class ProjectsViewSet(viewsets.ModelViewSet):
             return ProjectListSerializer
         return ProjectDetailSerializer
 
+    @transaction.atomic
     def create(self, request, *args, **kwargs):
         if not request.user.is_professor:
             return Response(
@@ -39,29 +40,47 @@ class ProjectsViewSet(viewsets.ModelViewSet):
             )
         return super().create(request, *args, **kwargs)
 
+    # * Include user_instance in the serializer's validated_data
+    @transaction.atomic
+    def perform_create(self, serializer):
+        serializer.save(user_instance=self.request.user)
+
+    @transaction.atomic
+    def perform_update(self, serializer):
+        serializer.save(user_instance=self.request.user)
+
+    @transaction.atomic
     def update(self, request, *args, **kwargs):
         if not request.user.is_professor:
             return Response(
                 {"error": "Unauthorized action. Only professors can update projects"},
                 status=status.HTTP_403_FORBIDDEN,
             )
-        return super().create(request, *args, **kwargs)
+        return super().update(request, *args, **kwargs)
 
+    @transaction.atomic
     def partial_update(self, request, *args, **kwargs):
         if not request.user.is_professor:
             return Response(
                 {"error": "Unauthorized action. Only professors can update projects"},
                 status=status.HTTP_403_FORBIDDEN,
             )
-        return super().create(request, *args, **kwargs)
+        return super().partial_update(request, *args, **kwargs)
 
+    @transaction.atomic
     def destroy(self, request, *args, **kwargs):
         if not request.user.is_professor:
             return Response(
                 {"error": "Unauthorized action. Only professors can delete projects"},
                 status=status.HTTP_403_FORBIDDEN,
             )
-        return super().create(request, *args, **kwargs)
+        user_instance = request.user
+        project_instance = self.get_object()
+        try:
+            user_instance.professorprofile.delete_project(project=project_instance)
+        except ValidationError as e:
+            return Response(e)
+        return Response("Project has been deleted", status=status.HTTP_200_OK)
 
     # TODO: Handle student already being enrolled in a project
     # TODO: Handle Team lead and professors adding students to project
@@ -90,8 +109,14 @@ class ProjectsViewSet(viewsets.ModelViewSet):
 
         if user_instance == request.user:
             if user_instance.studentprofile.project:
-                user_instance.studentprofile.leave_project()
-            user_instance.studentprofile.join_project(project_instance)
+                try:
+                    user_instance.studentprofile.leave_project()
+                except ValidationError as e:
+                    return Response(e)
+            try:
+                user_instance.studentprofile.join_project(project_instance)
+            except ValidationError as e:
+                return Response(e)
             project_instance.refresh_from_db()
         else:
             return Response(
@@ -141,7 +166,10 @@ class ProjectsViewSet(viewsets.ModelViewSet):
             )
 
         if user_instance == request.user:
-            user_instance.studentprofile.leave_project()
+            try:
+                user_instance.studentprofile.leave_project()
+            except ValidationError as e:
+                return Response(e)
             project_instance.refresh_from_db()
         else:
             return Response(
@@ -158,4 +186,102 @@ class ProjectsViewSet(viewsets.ModelViewSet):
                 "open_slots": project_serializer_instance.data["open_slots"],
             },
             status=status.HTTP_200_OK,
+        )
+
+    @action(detail=True, methods=["post"], url_path="professors")
+    @transaction.atomic
+    def professor_join_project(self, request, pk=None):
+        user_id = request.data.get("user_id")
+
+        if not user_id:
+            return Response(
+                {"error": "Valid user ID is required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            UUID(user_id, version=4)
+        except ValueError:
+            return Response(
+                {"error": "Invalid user ID format."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user_instance = get_object_or_404(CustomUser, pk=user_id)
+        if not user_instance.is_professor:
+            return Response(
+                "User is not a professor", status=status.HTTP_401_UNAUTHORIZED
+            )
+        project_instance = self.get_object()
+
+        if user_instance == request.user:
+            try:
+                user_instance.professorprofile.join_project(project_instance)
+            except ValidationError as e:
+                return Response(e)
+            project_instance.refresh_from_db()
+        else:
+            return Response(
+                {
+                    "error": "Unauthorized action. You cannot add another user to a project."
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        return Response(
+            "You have successfully joined the project.", status=status.HTTP_200_OK
+        )
+
+    @action(
+        detail=False,
+        methods=["delete"],
+        url_path="<int:pk>/professors/<str:user_id>/",
+    )
+    @transaction.atomic
+    def professor_leave_project(self, request, pk, user_id):
+        if not user_id:
+            return Response(
+                {"error": "Valid user ID is required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            UUID(user_id, version=4)
+        except ValueError:
+            return Response(
+                {"error": "Invalid user ID format."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user_instance = get_object_or_404(CustomUser, pk=user_id)
+
+        if not user_instance.is_professor:
+            return Response(
+                "User is not a professor", status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        project_instance = self.get_object()
+
+        if not project_instance:
+            return Response(
+                {"error": "Professor is not associated with any projects."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if user_instance == request.user:
+            try:
+                user_instance.professorprofile.leave_project(project_instance)
+            except ValidationError as e:
+                return Response(e)
+            project_instance.refresh_from_db()
+        else:
+            return Response(
+                {
+                    "error": "Unauthorized action. You cannot remove another user from a project."
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        return Response(
+            "You have successfully left the project.", status=status.HTTP_200_OK
         )
